a12016f107134ce7b89cfa27bdbdc1c94b176ec1
JayKhan4Real <68512622+JayKhan4Real@users.noreply.github.com>
2021-01-29 12:31:22 -0500

    Add files via upload

---
+++ AP/Apple Pie/Apple Pie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Apple Pie
+//
+//  Created by Fariha Hussain on 2/24/20.
+//  Copyright © 2020 PretendCo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Apple Pie/Apple Pie/Apple Pie/Game.swift
@@ -0,0 +1,37 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Fariha Hussain on 6/7/20.
+//  Copyright © 2020 PretendCo. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
+    
+    
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+}
+
+
